9b45cb670621b882fb7d4a10cecbf4df8b02dd47
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2024-05-03 14:46:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const meuArray = [2,3,4,18];
+
+console.log('primeiro elemento:', meuArray[0]);
+
+meuArray.push('novo');
+meuArray.unshift('primeiro');
+
+console.log('primeiro elemento:', meuArray[0]);
