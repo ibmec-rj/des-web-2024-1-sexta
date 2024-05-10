60e98d23200464e5796ca13b647fdd350f873861
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2024-05-10 15:16:22 -0300

    implementando filtro

--- assíncrono/script.js
+++ assíncrono/script.js
@@ -1,4 +1,17 @@
-const container = document.getElementById('container');
+const container = document.createElement('container');
+container.style.display = 'flex';
+container.style.gap = '.5em';
+container.style.justifyContent = 'center';
+container.style.flexWrap = 'wrap';
+
+const divPesquisa = document.createElement('div');
+divPesquisa.style.textAlign = 'center';
+const inputPesquisa = document.createElement('input');
+inputPesquisa.type = 'text';
+divPesquisa.appendChild(inputPesquisa);
+
+document.body.appendChild(divPesquisa);
+document.body.appendChild(container);
 
 
 const constroiCard = ( atleta ) => {
@@ -7,8 +20,8 @@ const constroiCard = ( atleta ) => {
     divCard.style.display = 'grid';
     divCard.style.width = 'fit-content';
     divCard.style.padding = '.5rem';
-    divCard.style.border = '1px solid black';
-    divCard.style.borderRadius = '5%';
+    divCard.style.border = '2px solid black';
+    //divCard.style.borderRadius = '5%';
     divCard.style.gridTemplateColumns = "10rem 20rem";
     divCard.style.gridTemplateAreas = "'a1 a2' 'a3 a3' 'a4 a4'";
 
@@ -16,23 +29,44 @@ const constroiCard = ( atleta ) => {
     const imagem = document.createElement('img');
     imagem.src = atleta.imagem;
     imagem.alt = atleta.nome;
+    imagem.style = `
+        grid-area: a1;
+        height: 8rem;
+        width: 8rem;
+        object-fit: cover;
+        object-position: top;
+        border-radius: 50%;
+    `;
 
     const titulo = document.createElement('div');
     titulo.className = 'titulo';
+    titulo.style.gridArea = 'a2';
+    titulo.style.display = 'flex';
+    titulo.style.flexDirection = 'column';
+    titulo.style.alignItems = 'center';
+    titulo.style.justifyContent = 'center';
+    titulo.style.fontWeight = 'bold'
+    titulo.style.fontFamily = 'sans-serif';
+    titulo.style.textTransform = 'uppercase';
 
     const pPosicao = document.createElement('p');
     pPosicao.innerHTML = atleta.posicao;
+    
 
     const pNome = document.createElement('p');
     pNome.innerHTML = atleta.nome;
+    pNome.style.fontSize = '1.3rem';
+    
 
     const pDescri = document.createElement('p');
     pDescri.className = 'descri'
     pDescri.innerHTML = atleta.descricao;
+    pDescri.style.gridArea = 'a3';
 
     const pNasci = document.createElement('p');
     pNasci.className = 'nasci'
     pNasci.innerHTML = atleta.nascimento;
+    pNasci.style.gridArea = 'a4';
 
     divCard.appendChild(imagem);
     
@@ -53,4 +87,17 @@ feminino.forEach(
     }
 )
 
+inputPesquisa.onchange = (e) => {
+    const valor = e.target.value;
+    const resultado = feminino.filter(
+        (elemento) => elemento.nome.toLowerCase().includes(valor.toLowerCase())
+    )
 
+    container.innerHTML = '';
+
+    resultado.forEach(
+        (jogadora) => {
+            constroiCard(jogadora)
+        }
+    )
+}
